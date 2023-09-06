cad9391c978488fca24402cd755b3b8011994095
Luis Trejo <trejorias@gmail.com>
2023-09-06 15:02:45 -0600

    add nvim key map

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -286,6 +286,9 @@ vim.keymap.set('n', '<leader>p', '\"+p')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 --Normal mode in terminal TODO
 -- vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
